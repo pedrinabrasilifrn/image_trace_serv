c153c6a76cfdd0fb2d1d68cef7d03f5bca26a944
PedrinaBrasil <pedrinabrasil@gmail.com>
2022-03-21 09:20:41 -0300

    enviando nome do módulo no experimentos/questoes

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from cpanel.models import Experiment, Quest
+from cpanel.models import Experiment, Quest,Module
 
 
 # Serializers define the API representation.
@@ -9,10 +9,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'is_staff']
 
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['id', 'name']
+
 class QuestSerializer(serializers.ModelSerializer):
+    module = ModuleSerializer(read_only=True, many=False)
     class Meta:
         model = Quest
-        fields = ['id', 'name', 'txt', 'timer', 'correct_op', 'wrong_op', 'speed', 'noise', 'upsidedown', 'videofile']
+        fields = ['id', 'name', 'module', 'txt', 'timer', 'correct_op', 'wrong_op', 'speed', 'noise', 'upsidedown', 'videofile']
 
 class ExperimentSerializer(serializers.ModelSerializer):
     quests = QuestSerializer(read_only=True, many=True)

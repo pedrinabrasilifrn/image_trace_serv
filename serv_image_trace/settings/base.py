4e3924e55117bcc78fa599a0da51fe497de8c99e
Pedrina Celia Brasil <2131716@ifrn.local>
2022-07-14 13:29:02 -0300

    mocao static para root

--- serv_image_trace/settings/base.py
+++ serv_image_trace/settings/base.py
@@ -133,7 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 
 # Default primary key field type
